bb5d1bc8c848517bdaf6d58c05e55a364cccbf9a
xmduhan <xmduhan@gmail.com>
2015-03-11 19:08:12 +0800

    ...

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -2,10 +2,9 @@
 #include "hello.h"
 
 int main(){
-	// 测试调用CTP的API
-	// 这里访问了CTP的库函数，如果程序能过正常编译并执行，说明头文件引用及库连接正常 	
-	CThostFtdcMdApi *pUserApi = CThostFtdcMdApi::CreateFtdcMdApi();
-    printf("Hello CTP!\n");
+		// 测试调用CTP的API
+		// 这里访问了CTP的库函数，如果程序能过正常编译并执行，说明头文件引用及库连接正常
+		CThostFtdcMdApi *pUserApi = CThostFtdcMdApi::CreateFtdcMdApi();
+		printf("Hello CTP!\n");
     return 0;
 }
-
